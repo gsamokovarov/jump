9630c87edacf442d52267c7c54ee1f5a79593b40
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-04 23:40:34 +0300

    Prototype the config package
    Not sure if this is what I want, but I'm getting there.

--- config/config.go
+++ config/config.go
@@ -1,7 +1,90 @@
 package config
 
-func CreateOrLoad(configPath string) error {
-	configPath = configPath[:]
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path"
 
-	return nil
+	"github.com/gsamokovarov/jump/scoring"
+)
+
+// The default configuration.
+var Default Config
+
+// A config represents the config directory and the file that stores the score
+// values.
+type Config struct {
+	Dir    string
+	Scores *os.File
+}
+
+// Write the input scoring entries to a file.
+//
+// Formats them in a similar format of autojump.
+func (c *Config) WriteEntries(entries *scoring.Entries) error {
+	buffer := []byte{}
+
+	for _, entry := range *entries {
+		buffer = append(buffer, formatEntry(entry)...)
+	}
+
+	_, err := c.Scores.Write(buffer)
+
+	return err
+}
+
+func formatEntry(entry scoring.Entry) []byte {
+	return []byte(fmt.Sprintf("%f %s\n", entry.CalculateScore(), entry.Path))
+}
+
+// This is the default score name.
+const defaultScoreFile = "scores.txt"
+
+// Setups the config folder from a directory path.
+//
+// If the directories don't already exists, they are created and if the score
+// file is present, it is loaded.
+func Setup(dir string) (*Config, error) {
+	// We get the directory check for free form os.MkdirAll.
+	if err := os.MkdirAll(dir, os.ModeDir); err != nil {
+		return nil, err
+	}
+
+	dirPath := path.Join(dir, defaultScoreFile)
+	scores, err := os.OpenFile(dirPath, os.O_APPEND, os.FileMode(0644))
+
+	if err == nil {
+		return nil, err
+	} else {
+		return &Config{dir, scores}, nil
+	}
+}
+
+func SetupDefault(dir string) error {
+	dir, err := normalizeDir(dir)
+	if err != nil {
+		return err
+	}
+
+	config, err := Setup(dir)
+	if err == nil {
+		Default = *config
+	}
+
+	return err
+}
+
+func normalizeDir(dir string) (string, error) {
+	if len(dir) == 0 {
+		usr, err := user.Current()
+		if err != nil {
+			return dir, err
+		}
+
+		homeDir := usr.HomeDir
+		return path.Join(homeDir, "jump"), nil
+	}
+
+	return dir, nil
 }
--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 
 	"github.com/gsamokovarov/jump/cli"
 	_ "github.com/gsamokovarov/jump/cmd"
+	"github.com/gsamokovarov/jump/config"
 )
 
 func main() {
 	args := cli.ParseArgs(os.Args)
+
+	if err := config.SetupDefault(os.Getenv("JUMP_HOME")); err != nil {
+		panic(fmt.Sprintf("bug: %s", err.Error()))
+	}
 
 	if err := cli.DispatchCommand(args, "--help"); err != nil {
 		panic(fmt.Sprintf("bug: %s", err.Error()))
