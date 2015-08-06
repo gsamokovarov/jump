546aa27b0bc614ae9d1d4658fb6e2df8deb4f17f
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-06 23:57:24 +0300

    Fix the cli tests

--- cli/command_test.go
+++ cli/command_test.go
@@ -1,25 +1,28 @@
 package cli
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/gsamokovarov/jump/config"
+)
 
 func TestDispatchCommand(t *testing.T) {
-	executed := false
-	RegisterCommand("test", "A testing command.", func([]string) { executed = true })
+	RegisterCommand("test", "A testing command.", func(Args, *config.Config) {})
 
 	args := Args([]string{"test"})
-	if err := DispatchCommand(args, "default"); err == nil && executed == true {
+	if _, err := DispatchCommand(args, "default"); err == nil {
 		t.Errorf("Expected an error on missing registered default command")
 	}
 
-	RegisterCommand("default", "A testing command.", func([]string) {})
+	RegisterCommand("default", "A testing command.", func(Args, *config.Config) {})
 
-	if _ = DispatchCommand(args, "default"); executed == false {
+	if command, _ := DispatchCommand(args, "default"); command.Name != "test" {
 		t.Errorf("Expected test command to be dispatched and executed")
 	}
 }
 
 func TestCommand(t *testing.T) {
-	command := &Command{"--test", "Testing command.", func([]string) {}}
+	command := &Command{"--test", "Testing command.", func(Args, *config.Config) {}}
 
 	if !command.IsOption() {
 		t.Errorf("Expected --test command to be an option")
