998f427960755692f7d02c8be44e848f468f2e69
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-08 19:48:05 +0300

    Test Entry UpdateScore method

--- scoring/entry_test.go
+++ scoring/entry_test.go
@@ -9,3 +9,12 @@ func TestEntriesCalculateScore(t *testing.T) {
 		t.Errorf("Expected entry.CalculateScore to be %f, got %f", expected, got)
 	}
 }
+
+func TestEntriesUpdateScore(t *testing.T) {
+	entry := Entry{"/foo", NewScore()}
+	entry.UpdateScore()
+
+	if got, expected := entry.Score.Weight, int64(2); got != expected {
+		t.Errorf("Expected entry.UpdateScore Weight to be %f, got %f", expected, got)
+	}
+}
