7d555b034c53c439c63b358e30188b9afa4102af
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-10 23:27:29 +0300

    Add a Entry.String method for debugging
    println ftw.

--- scoring/entry.go
+++ scoring/entry.go
@@ -1,5 +1,7 @@
 package scoring
 
+import "fmt"
+
 // An entry represents a path and a score.
 type Entry struct {
 	Path  string
@@ -14,6 +16,10 @@ func (e *Entry) UpdateScore() {
 // Calculates the score for an entry.
 func (e *Entry) CalculateScore() float64 {
 	return e.Score.Calculate()
+}
+
+func (e *Entry) String() string {
+	return fmt.Sprintf("{%s %s}", e.Path, e.Score)
 }
 
 // Create a new entry with the specified path. The score is created with
