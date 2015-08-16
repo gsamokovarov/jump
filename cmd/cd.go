261100ed7dca0304e9cd1fe1e07fd4d65cbad23e
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-16 13:27:43 +0300

    Initial implementation of the cd command

---
+++ cmd/cd.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"path/filepath"
+	"sort"
+
+	"github.com/gsamokovarov/jump/cli"
+	"github.com/gsamokovarov/jump/config"
+	"github.com/gsamokovarov/jump/lcs"
+	"github.com/gsamokovarov/jump/scoring"
+)
+
+type fuzzyEntries struct {
+	entries scoring.Entries
+	target  string
+}
+
+func (fe fuzzyEntries) Len() int {
+	return len(fe.entries)
+}
+
+func (fe fuzzyEntries) Swap(i, j int) {
+	fe.entries[i], fe.entries[j] = fe.entries[j], fe.entries[i]
+}
+
+func (fe fuzzyEntries) Less(i, j int) bool {
+	iPath := filepath.Base(fe.entries[i].Path)
+	jPath := filepath.Base(fe.entries[j].Path)
+
+	return lcs.Length(iPath, fe.target) >= lcs.Length(jPath, fe.target)
+}
+
+func (fe fuzzyEntries) Choose() (entry *scoring.Entry, empty bool) {
+	sort.Sort(fe)
+
+	if fe.Len() == 0 {
+		return nil, true
+	}
+
+	return &fe.entries[0], false
+}
+
+func cdCmd(args cli.Args, conf *config.Config) {
+	if len(args) == 0 {
+		return
+	}
+
+	entries := fuzzyEntries{conf.ReadEntries(), args.CommandName()}
+	if entry, empty := entries.Choose(); !empty {
+		cli.Outf("%s\n", entry.Path)
+	}
+}
+
+func init() {
+	cli.RegisterCommand("cd", "Fuzzy match a directory to jump to.", cdCmd)
+}
