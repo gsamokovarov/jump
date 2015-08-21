5b3ae20ee919875b71c01ab4edc1b12bc88968ce
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-22 00:02:02 +0300

    Extract scoring.FuzzyEntries out of cmd/cd

--- cmd/cd.go
+++ cmd/cd.go
@@ -1,57 +1,23 @@
 package cmd
 
 import (
-	"sort"
-	"strings"
-
 	"github.com/gsamokovarov/jump/cli"
 	"github.com/gsamokovarov/jump/config"
-	"github.com/gsamokovarov/jump/lcs"
 	"github.com/gsamokovarov/jump/scoring"
 )
-
-type fuzzyEntries struct {
-	entries scoring.Entries
-	target  string
-}
-
-func (fe fuzzyEntries) Len() int {
-	return len(fe.entries)
-}
-
-func (fe fuzzyEntries) Swap(i, j int) {
-	fe.entries[i], fe.entries[j] = fe.entries[j], fe.entries[i]
-}
-
-func (fe fuzzyEntries) Less(i, j int) bool {
-	iPath := strings.ToLower(fe.entries[i].BasePath())
-	jPath := strings.ToLower(fe.entries[j].BasePath())
-
-	return lcs.Length(iPath, fe.target) >= lcs.Length(jPath, fe.target)
-}
-
-func (fe fuzzyEntries) Choose() (entry *scoring.Entry, empty bool) {
-	sort.Sort(fe)
-
-	if fe.Len() == 0 {
-		return nil, true
-	}
-
-	return &fe.entries[0], false
-}
 
 func cdCmd(args cli.Args, conf *config.Config) {
 	if len(args) == 0 {
 		return
 	}
 
-	rawEntries, err := conf.ReadEntries()
+	entries, err := conf.ReadEntries()
 	if err != nil {
 		cli.Exitf(1, "%s\n", err)
 	}
 
-	entries := fuzzyEntries{rawEntries, args.CommandName()}
-	if entry, empty := entries.Choose(); !empty {
+	fuzzyEntries := scoring.NewFuzzyEntries(entries, args.CommandName())
+	if entry, empty := fuzzyEntries.Select(); !empty {
 		cli.Outf("%s\n", entry.Path)
 	}
 }
---
+++ scoring/fuzzy_entries.go
@@ -0,0 +1,49 @@
+package scoring
+
+import (
+	"sort"
+	"strings"
+
+	"github.com/gsamokovarov/jump/lcs"
+)
+
+type FuzzyEntries struct {
+	Entries
+	Target string
+}
+
+// Less compares the Longest Subsequence Length between the Target string and
+// every entry. The entries with greater LCS come first.
+func (fe FuzzyEntries) Less(i, j int) bool {
+	iPath := strings.ToLower(fe.Entries[i].BasePath())
+	jPath := strings.ToLower(fe.Entries[j].BasePath())
+
+	return lcs.Length(iPath, fe.Target) >= lcs.Length(jPath, fe.Target)
+}
+
+func (fe FuzzyEntries) Sort() {
+	// Leaving this, when fe.Sort() is called, the Entries.Sort method will be
+	// call. In its context, the receiver is Entries, therefore, Entries.Less
+	// and not FuzzyEntries.Less will be called during sorting.
+	sort.Sort(fe)
+}
+
+// Select selects the entry with greatest LCS score.
+func (fe FuzzyEntries) Select() (entry *Entry, empty bool) {
+	fe.Sort()
+
+	if fe.Len() == 0 {
+		return nil, true
+	}
+
+	return &fe.Entries[0], false
+}
+
+// NewFuzzyEntries converts a FuzzyEntries and a target string to a
+// FuzzyEntries struct.
+//
+// Entries is expected to be sorted before creating the FuzzyEntries. This
+// gives us the best match. This is not enforced, however.
+func NewFuzzyEntries(entries Entries, target string) *FuzzyEntries {
+	return &FuzzyEntries{entries, target}
+}
