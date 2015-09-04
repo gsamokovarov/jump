ed3a4206b26883c775e450d9180953809252fc19
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-09-04 16:22:20 +0300

    Drop the big used-to-be-for-a-bug comment
    Just mention the entries are expected to be sorted in ASC and that's it.

--- scoring/fuzzy_entries.go
+++ scoring/fuzzy_entries.go
@@ -43,18 +43,8 @@ func (fe FuzzyEntries) Select() (entry *Entry, empty bool) {
 // NewFuzzyEntries converts a FuzzyEntries and a target string to a
 // FuzzyEntries struct.
 //
-// Entries is expected to be sorted before creating the FuzzyEntries. This
-// gives us the best match. This is not enforced, however.
+// Entries is expected to be sorted in ASC before creating the FuzzyEntries.
+// This gives us the best match. This is not enforced, however.
 func NewFuzzyEntries(entries Entries, target string) *FuzzyEntries {
-	// Given that the entires are sorted in ASC order. This will make the
-	// directories with higher score pop, as they will come later on in the
-	// FuzzyEntries sort and get pushed to the beginning of the list.
-	//
-	// Say you have /Users/genad/Development/jump with a weight of 70 and
-	// /Users/genadi/Development/homebrew-jump with a weight of 3. If we haven't
-	// reverse input entries, we would have selected the directory of
-	// /Users/genadi/Development/homebrew-jump, because it has the same
-	// lcs.Length, but came after the proper directory we want in initial
-	// entries, so it got pushed to the beginning of slice.
 	return &FuzzyEntries{entries, target}
 }
