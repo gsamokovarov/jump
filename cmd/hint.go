9b9226aff53ea74fca8a2670acceae5330187b04
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-09-27 12:21:08 +0200

    Hint all the entries if no arguments given to jump hint

--- cmd/hint.go
+++ cmd/hint.go
@@ -1,24 +1,34 @@
 package cmd
 
 import (
+	"sort"
+
 	"github.com/gsamokovarov/jump/cli"
 	"github.com/gsamokovarov/jump/config"
 	"github.com/gsamokovarov/jump/scoring"
 )
 
 func hintCmd(args cli.Args, conf *config.Config) {
-	if len(args) == 0 {
-		return
-	}
+	var hints scoring.Entries
 
 	entries, err := conf.ReadEntries()
 	if err != nil {
 		cli.Exitf(1, "%s\n", err)
 	}
 
-	fuzzyEntries := scoring.NewFuzzyEntries(entries, args.CommandName())
-	fuzzyEntries.Sort()
-	for _, entry := range fuzzyEntries.Entries {
+	if len(args) == 0 {
+		// We usually keep them reversely sort to optimize the fuzzy search.
+		sort.Sort(sort.Reverse(entries))
+
+		hints = entries
+	} else {
+		fuzzyEntries := scoring.NewFuzzyEntries(entries, args.CommandName())
+		fuzzyEntries.Sort()
+
+		hints = fuzzyEntries.Entries
+	}
+
+	for _, entry := range hints {
 		cli.Outf("%s\n", entry.Path)
 	}
 }
