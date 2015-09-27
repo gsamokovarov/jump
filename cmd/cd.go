5747cc8b3dbcddb3c0464b40963c8370061f717b
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-09-27 12:29:40 +0200

    If the input to jump cd is an absolute path, go to it
    This no longer triggers a fuzzy search, which may get you a bad result
    because of the scoring.

--- cmd/cd.go
+++ cmd/cd.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"path/filepath"
+
 	"github.com/gsamokovarov/jump/cli"
 	"github.com/gsamokovarov/jump/config"
 	"github.com/gsamokovarov/jump/scoring"
@@ -11,14 +13,20 @@ func cdCmd(args cli.Args, conf *config.Config) {
 		return
 	}
 
+	commandName := args.CommandName()
 	entries, err := conf.ReadEntries()
 	if err != nil {
 		cli.Exitf(1, "%s\n", err)
 	}
 
-	fuzzyEntries := scoring.NewFuzzyEntries(entries, args.CommandName())
-	if entry, empty := fuzzyEntries.Select(); !empty {
-		cli.Outf("%s\n", entry.Path)
+	if filepath.IsAbs(commandName) {
+		// If an auto-completion triggered a full path, just go there.
+		cli.Outf("%s\n", commandName)
+	} else {
+		fuzzyEntries := scoring.NewFuzzyEntries(entries, args.CommandName())
+		if entry, empty := fuzzyEntries.Select(); !empty {
+			cli.Outf("%s\n", entry.Path)
+		}
 	}
 }
 
