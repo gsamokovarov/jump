2b276f3331e6a7e94c9c5b6cd6945a97b3cf6f6a
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-11-25 11:50:22 +0200

    Extract the entry related methods in config/entries.go

--- config/config.go
+++ config/config.go
@@ -1,13 +1,9 @@
 package config
 
 import (
-	"encoding/json"
-	"io"
 	"os"
 	"os/user"
 	"path/filepath"
-
-	"github.com/gsamokovarov/jump/scoring"
 )
 
 const (
@@ -22,52 +18,6 @@ type Config struct {
 	Dir    string
 	Scores string
 	Search string
-}
-
-// Read entries returns the current entries for the config.
-//
-// If the scores file is empty, the returned entries are empty.
-func (c *Config) ReadEntries() (scoring.Entries, error) {
-	var entries scoring.Entries
-
-	scoresFile, err := c.scoresFile()
-	if err != nil {
-		return entries, nil
-	}
-
-	defer scoresFile.Close()
-
-	decoder := json.NewDecoder(scoresFile)
-	for {
-		if err := decoder.Decode(&entries); err == io.EOF {
-			break
-		} else if err != nil {
-			return entries, err
-		}
-	}
-
-	return entries, nil
-}
-
-// Write the input scoring entries to a file.
-//
-// Sorts the entries before writing them to disk.
-func (c *Config) WriteEntries(entries scoring.Entries) error {
-	scoresFile, err := c.scoresFile()
-	if err != nil {
-		return err
-	}
-
-	defer scoresFile.Close()
-
-	if err := scoresFile.Truncate(0); err != nil {
-		return err
-	}
-
-	entries.Sort()
-	encoder := json.NewEncoder(scoresFile)
-
-	return encoder.Encode(&entries)
 }
 
 // Returns a file object for the saved scores path.
---
+++ config/entries.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/gsamokovarov/jump/scoring"
+)
+
+// Read entries returns the current entries for the config.
+//
+// If the scores file is empty, the returned entries are empty.
+func (c *Config) ReadEntries() (scoring.Entries, error) {
+	var entries scoring.Entries
+
+	scoresFile, err := c.scoresFile()
+	if err != nil {
+		return entries, nil
+	}
+
+	defer scoresFile.Close()
+
+	decoder := json.NewDecoder(scoresFile)
+	for {
+		if err := decoder.Decode(&entries); err == io.EOF {
+			break
+		} else if err != nil {
+			return entries, err
+		}
+	}
+
+	return entries, nil
+}
+
+// Write the input scoring entries to a file.
+//
+// Sorts the entries before writing them to disk.
+func (c *Config) WriteEntries(entries scoring.Entries) error {
+	scoresFile, err := c.scoresFile()
+	if err != nil {
+		return err
+	}
+
+	defer scoresFile.Close()
+
+	if err := scoresFile.Truncate(0); err != nil {
+		return err
+	}
+
+	entries.Sort()
+	encoder := json.NewEncoder(scoresFile)
+
+	return encoder.Encode(&entries)
+}
