301cbb2029dbc18690e073aabe99521ce6ea651d
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-18 12:10:59 +0300

    Start with a bit of cdCmd tests

---
+++ cmd/cd_test.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+	p "path"
+	"strings"
+	"testing"
+
+	"github.com/gsamokovarov/jump/cli"
+	s "github.com/gsamokovarov/jump/scoring"
+)
+
+func Test_cdCmd(t *testing.T) {
+	conf := &testConfig{
+		Entries: s.Entries{
+			s.Entry{p.Join(td, "web-console"), &s.Score{Weight: 100, Age: s.Now}},
+			s.Entry{p.Join(td, "/client/website"), &s.Score{Weight: 90, Age: s.Now}},
+		},
+	}
+
+	output := capture(&os.Stdout, func() {
+		cdCmd(cli.Args{"wc"}, conf)
+	})
+
+	expectedPath := p.Join(td, "web-console")
+
+	if !strings.Contains(output, expectedPath) {
+		t.Fatalf("Expected path to be %s, got %s", expectedPath, output)
+	}
+}
---
+++ cmd/cmd_test.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+
+	"github.com/gsamokovarov/jump/config"
+	"github.com/gsamokovarov/jump/scoring"
+)
+
+var td string
+
+type testConfig struct {
+	Entries scoring.Entries
+	Search  config.Search
+	Pin     string
+}
+
+func (c *testConfig) ReadEntries() (*scoring.Entries, error) {
+	return &c.Entries, nil
+}
+
+func (c *testConfig) WriteEntries(entries *scoring.Entries) error {
+	c.Entries = *entries
+	return nil
+}
+
+func (c *testConfig) ReadSearch() config.Search {
+	return c.Search
+}
+
+func (c *testConfig) WriteSearch(term string, index int) error {
+	c.Search.Term = term
+	c.Search.Index = index
+	return nil
+}
+
+func (c *testConfig) FindPin(term string) (string, bool) {
+	return c.Pin, c.Pin != ""
+}
+
+func (c *testConfig) WritePin(_, value string) error {
+	c.Pin = value
+	return nil
+}
+
+func capture(stream **os.File, fn func()) string {
+	rescue := *stream
+	r, w, _ := os.Pipe()
+
+	*stream = w
+	defer func() {
+		*stream = rescue
+	}()
+
+	fn()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+
+	return string(out)
+}
+
+func init() {
+	cwd, _ := os.Getwd()
+	td = path.Join(cwd, "testdata")
+}
