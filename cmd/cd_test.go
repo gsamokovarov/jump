8b3adb488c24aeb1b25ffbc4cb73ce5219630bb3
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-29 16:02:27 +0300

    Introduce cdCmd absolutePath test

--- cmd/cd_test.go
+++ cmd/cd_test.go
@@ -28,3 +28,22 @@ func Test_cdCmd(t *testing.T) {
 		t.Fatalf("Expected path to be %s, got %s", expectedPath, output)
 	}
 }
+
+func Test_cdCmd_absolutePath(t *testing.T) {
+	conf := &testConfig{
+		Entries: s.Entries{
+			s.Entry{p.Join(td, "web-console"), &s.Score{Weight: 100, Age: s.Now}},
+			s.Entry{p.Join(td, "/client/website"), &s.Score{Weight: 90, Age: s.Now}},
+		},
+	}
+
+	output := capture(&os.Stdout, func() {
+		cdCmd(cli.Args{"/absolute/path"}, conf)
+	})
+
+	expectedPath := "/absolute/path\n"
+
+	if output != expectedPath {
+		t.Fatalf("Expected path to be %s, got %s", expectedPath, output)
+	}
+}
