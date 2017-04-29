25038c83d0c23f90add206b6c7f3e4af88ed09cb
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-29 16:03:40 +0300

    Test the top command

---
+++ cmd/top_test.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"os"
+	p "path"
+	"strings"
+	"testing"
+
+	"github.com/gsamokovarov/jump/cli"
+	s "github.com/gsamokovarov/jump/scoring"
+)
+
+func Test_topCmd(t *testing.T) {
+	wc := p.Join(td, "web-console")
+	web := p.Join(td, "/client/website")
+
+	conf := &testConfig{
+		Entries: s.Entries{
+			s.Entry{wc, &s.Score{Weight: 100, Age: s.Now}},
+			s.Entry{web, &s.Score{Weight: 90, Age: s.Now}},
+		},
+	}
+
+	output := capture(&os.Stdout, func() {
+		topCmd(cli.Args{}, conf)
+	})
+
+	lines := strings.Split(output, "\n")
+
+	if lines[0] != wc {
+		t.Fatalf("Expected first line to be %s, got %s", wc, lines[0])
+	}
+
+	if lines[1] != web {
+		t.Fatalf("Expected first line to be %s, got %s", web, lines[1])
+	}
+}
