cee85ea466d597f1471fc612c5717540e8ee1208
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-04-23 14:46:10 +0300

    Use gsamokovarov/assert in the shell testing

--- shell/shell_test.go
+++ shell/shell_test.go
@@ -2,57 +2,31 @@ package shell
 
 import (
 	"testing"
+
+	"github.com/gsamokovarov/assert"
 )
 
 func TestGuessFish(t *testing.T) {
-	if Guess("/usr/local/bin/fish") != Fish {
-		t.Errorf("Expected /usr/local/bin/fish to match the fish shell")
-	}
+	assert.Equal(t, Fish, Guess("/usr/local/bin/fish"))
 }
 
 func TestFishCompiles(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			t.Errorf("Expected Fish to be a valid template: \n%s", r)
-		}
-	}()
-
 	Fish.MustCompile("j")
 }
 
 func TestGuessZsh(t *testing.T) {
-	if Guess("/bin/zsh") != Zsh {
-		t.Errorf("Expected /bin/zsh to match the zsh shell")
-	}
+	assert.Equal(t, Zsh, Guess("/usr/zsh"))
 }
 
 func TestZshCompiles(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			t.Errorf("Expected Zsh to be a valid template: \n%s", r)
-		}
-	}()
-
 	Zsh.MustCompile("j")
 }
 
 func TestGuessBash(t *testing.T) {
-	if Guess("/bin/bash") != Bash {
-		t.Errorf("Expected /bin/bash to match the bash shell")
-	}
-
-	if Guess("/bin/sh") != Bash {
-		// Its the most common one so fullback to it.
-		t.Errorf("Expected unknown shells to match the bash shell")
-	}
+	assert.Equal(t, Bash, Guess("/bin/bash"))
+	assert.Equal(t, Bash, Guess("/bin/sh"))
 }
 
 func TestBashCompiles(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			t.Errorf("Expected Bash to be a valid template: \n%s", r)
-		}
-	}()
-
 	Bash.MustCompile("j")
 }
