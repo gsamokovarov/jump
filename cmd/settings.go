820792595f9987a990ce34cc7de1edcac329208c
Genadi Samokovarov <gsamokovarov@gmail.com>
2020-01-30 20:58:56 +0200

    Narrow-down the jump settings usage test

--- cmd/settings.go
+++ cmd/settings.go
@@ -7,32 +7,33 @@ import (
 
 const settingsUsage = `Usage: jump settings --setting[=value]
 
-Jump is opinionated and we would recommend you to stick to the sweet
-hand-tuned defaults we have provided after years of research, however,
-we provide a few options few settings that may be useful to hand-tune
-yourself:
+Jump is opinionated and we would recommend you to stick to the
+sweet hand-tuned defaults we have provided after years of
+research, however, we provide a few options few settings that
+may be useful to hand-tune yourself:
 
 --space (values: slash (default), ignore)
 
-  The calls "j parent child" and "j parent/child" are equivalent by
-  default because spaces are treated as OS separators (/ in Unix). You
-  can choose to ignore spaces in searches by setting the "spaces" option
-  to "ignore":
+The calls "j parent child" and "j parent/child" are equivalent
+by default because spaces are treated as OS separators (/ in
+Unix). You can choose to ignore spaces in searches by setting
+the "spaces" option to "ignore":
 
     jump settings --space=ignore
 
 --preserve (values: false (default), true)
 
-  By default, landing in a directory that is no-longer available on disk
-  will cause jump to remove that directory from its database. If a jump
-  lands in unmounted drive, the changing of directory will timeout. This
-  is why this is turned off (false) by default.
+By default, landing in a directory that is no-longer available
+on disk will cause jump to remove that directory from its
+database. If a jump lands in unmounted drive, the changing of
+directory will timeout. This is why this is turned off (false)
+by default.
 
     jump settings --preserve=true
 
 --reset
 
-  Reset jump settings to their default values.
+Reset jump settings to their default values.
 
     jump settings --reset
 `
